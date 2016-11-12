85dd56f9d9bb72d181a89e0420dad217a4d65af7
Ross Hatton <Ross.Hatton@oregonstate.edu>
2016-11-11 19:31:30 -0800

    Removed Jacobian calculation from fatbackbone functions, which should speed up execution

--- ProgramFiles/v4.2/Utilities/curvature_mode_toolbox/fatbackbone_from_curvature_bases.m
+++ ProgramFiles/v4.2/Utilities/curvature_mode_toolbox/fatbackbone_from_curvature_bases.m
@@ -1,6 +1,6 @@
 function B = fatbackbone_from_curvature_bases(kappa_basis_input,r,L,width)
 
-	[h, J] = backbone_from_curvature_bases(kappa_basis_input,r,L);
+	[h] = backbone_from_curvature_bases(kappa_basis_input,r,L);
 	
 	
 	B = fatbackbone(h,L*[-.5 .5],width);
--- ProgramFiles/v4.2/Utilities/curvature_mode_toolbox/fatbackbone_from_general_curvature.m
+++ ProgramFiles/v4.2/Utilities/curvature_mode_toolbox/fatbackbone_from_general_curvature.m
@@ -1,6 +1,6 @@
 function B = fatbackbone_from_general_curvature(curvdef,cparams,L,width)
 
-	[h, J] = backbone_from_general_curvature(curvdef,cparams,L);
+	[h] = backbone_from_general_curvature(curvdef,cparams,L);
 	
 	
 	B = fatbackbone(h,L*[-.5 .5],width);
