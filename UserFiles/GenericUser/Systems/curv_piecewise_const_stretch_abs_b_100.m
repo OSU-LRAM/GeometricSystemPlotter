f1257af74453aaaa2448a3fd83bdd228298c6736
remaleyj <remaleyj@oregonstate.edu>
2017-12-20 17:34:37 -0800

    Added sample extensible system files

---
+++ UserFiles/GenericUser/Systems/curv_piecewise_const_stretch_abs_b_100.m
@@ -0,0 +1,59 @@
+function output = curv_piecewise_const_stretch_abs_b_100(params,mode)
+
+% 4/6/17 Jacquelin's attempt at a simple version of a curvature file, with
+% eventual goal of adding elongation mode
+
+% Turn params into a cell matrix
+% params = num2cell(params);
+
+switch mode
+    
+    case 'curvature'
+        
+        % kappas
+        % get alphas for use in heaviside function
+        a1 = params(1); 
+        a2 = params(2);
+        output = @(s) a1*heaviside(-s)+a2*heaviside(s);
+
+    case 'angle'
+        
+        % thetas
+        a1 = params(1); 
+        a2 = params(2);
+        output = @(s) a1*heaviside(-s).*s+a2*heaviside(s).*s;
+        
+    case 'dcurvature'
+        
+        % dk/da 
+        output = @(s) [heaviside(-s) heaviside(s)];
+            
+    case 'dcurvature_int'
+        
+        % int(dk/da)
+        % are the dimensions of these arrays correct?
+        output = @(s) [heaviside(-s).*s heaviside(s).*s];
+        
+    case 'stretch'
+        
+        % lambda term for elongation
+        b = 1.000000e-01;       
+        a1 = params(1);  % alphas
+        a2 = params(2);
+        e1 = b*abs(a1) + 1; % lambda, elongation factor
+        e2 = b*abs(a2) + 1;
+        output = @(s) e1*heaviside(-s)+e2*heaviside(s);
+        
+    case 'dstretch'
+        
+        % change in lambda
+        b = 1.000000e-01;       
+        a1 = params(1);  % alphas
+        a2 = params(2);
+        e1 = b*sign(a1); % dlambda, elongation factor change
+        e2 = b*sign(a2);
+        output = @(s) [e1*heaviside(-s) e2*heaviside(s)];
+                                   
+end
+
+end
---
+++ UserFiles/GenericUser/Systems/curv_serpenoid_stretch_abs_b_100.m
@@ -0,0 +1,99 @@
+% a01.*1c*oasb(s2(*ap1i**cso)s+(a22**psii*ns()2+*ap2i**ssi)n ( 2 * p i * s ) ) + 1
+function output = curv_serpenoid_stretch_abs_b_100(params,mode)
+
+% Turn params into a cell matrix
+params = num2cell(params);
+
+switch mode
+    
+    case 'curvature'
+
+		output = @(s) curv_fun(s,params{:});
+
+
+    case 'angle'
+        
+		output = @(s) int_curv_ds_fun(s,params{:});
+
+        
+    case 'dcurvature'
+        
+		output = @(s) d_curv_dp_fun(s,params{:});
+
+            
+    case 'dcurvature_int'
+        
+		output = @(s) int_d_curv_dp_ds_fun(s,params{:});
+
+
+    case 'stretch'
+
+		output = @(s) stretch_fun(s,params{:});
+
+    
+    case 'dstretch'
+
+		output = @(s) dstretch_fun(s,params{:});
+
+                                   
+end
+
+end
+
+function output = vector_to_list_input(funhandle,all_params)
+
+    all_params = num2cell(all_params);
+    
+    output = funhandle(all_params{:});
+    
+end
+
+
+function output = reshape_truncate_jacobian(J)
+
+    output = J(2:end)';
+    
+end
+
+function out1 = curv_fun(s,a1,a2)
+	t2 = s.*pi.*2.0;
+	out1 = a1.*cos(t2)+a2.*sin(t2);
+end
+
+
+function out1 = int_curv_ds_fun(s,a1,a2)
+	t2 = sin(s.*pi);
+	out1 = (a1.*sin(s.*pi.*2.0).*(1.0./2.0)+a2.*t2.^2)./pi;
+end
+
+
+function out1 = d_curv_dp_fun(s,a1,a2)
+	t2 = s.*pi.*2.0;
+	out1 = [cos(t2),sin(t2)];
+end
+
+
+function out1 = int_d_curv_dp_ds_fun(s,a1,a2)
+	t2 = 1.0./pi;
+	t3 = sin(s.*pi);
+	out1 = [t2.*sin(s.*pi.*2.0).*(1.0./2.0),t2.*t3.^2];
+end
+
+
+function out1 = stretch_fun(s,a1,a2)
+	t2 = s.*pi.*2.0;
+	out1 = abs(a1.*cos(t2)+a2.*sin(t2)).*1.0e-1+1.0;
+end
+
+
+function out1 = dstretch_fun(s,a1,a2)
+	t2 = s.*pi.*2.0;
+	t3 = cos(t2);
+	t4 = a1.*t3;
+	t5 = sin(t2);
+	t6 = a2.*t5;
+	t7 = t4+t6;
+	t8 = sign(t7);
+	out1 = [t3.*t8.*1.0e-1,t5.*t8.*1.0e-1];
+end
+
