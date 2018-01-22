d9be035779cbdb84b18f2aac84c860c527aa0013
remaleyj <remaleyj@oregonstate.edu>
2018-01-22 10:37:08 -0800

    Moved PropertyData files and modified sysplotter_config to save correct path.

--- ProgramFiles/sysplotter_config_fcns/OKbutton_Callback.m
+++ ProgramFiles/sysplotter_config_fcns/OKbutton_Callback.m
@@ -32,7 +32,7 @@ function OKbutton_Callback(hObject, eventdata, handles)
     sysplotterpath = pwd;
     
     % propertyfile
-    propertyfilepath = [datapath,get(handles.displayConfigFile,'String')];
+    propertyfilepath = [sysplotterpath,'\',get(handles.displayConfigFile,'String')];
 
     % Save the path info to a file for sysplotter to refer to
     save('sysplotter_config','inputpath','syspath','shchpath','stretchpath','datapath','HHpath','Refpointpath','Colorset','Colorpath','sysplotterpath','propertyfilepath');
