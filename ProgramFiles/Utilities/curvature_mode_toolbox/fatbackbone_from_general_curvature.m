39c1b8a051b96bb523aecf41a3bdbeb714cd5707
Quinten Konyn <qkonyn@gmail.com>
2018-07-13 16:15:01 -0700

    Update fatbackbone_from_general_curvature.m
    added support for the new from-system orientation option
    
    I haven't tested this because I haven't been able to get a system that uses a general curvature to work properly, and I don't think it's worth my time to figure that out. But the code is almost identical to the tested fatbackbone_from_curvature_bases.m so I think it's pretty safe to say it will work.
    
    If not, it definitely won't break the old functionality.

--- ProgramFiles/Utilities/curvature_mode_toolbox/fatbackbone_from_general_curvature.m
+++ ProgramFiles/Utilities/curvature_mode_toolbox/fatbackbone_from_general_curvature.m
@@ -3,6 +3,18 @@ function [B,h] = fatbackbone_from_general_curvature(curvdef,cparams,L,width,orie
     % Specify orientation as midpoint-tangent unless specified otherwise
     if ~exist('orientation','var')
         orientation = 'midpoint-tangent';
+    elseif or(isequal(orientation,'com-mean'), isequal(orientation,'midpoint-tangent'))
+        % do nothing; orientation is a valid option
+    else
+        % Get the user folder path
+        load('sysplotter_config.mat','inputpath');
+        % The hypothetical file path, supposing orientation is a system name
+        calcfilePath = strcat(inputpath,'\sysplotter_data\sysf_',orientation,'_calc.mat');
+        if exist(calcfilePath, 'file')
+            orientation = 'from-sys';
+        else
+            orientation = 'midpoint-tangent'; % illustrate_backbone_shapespace provides a warning about this
+        end
     end
     
     [h] = backbone_from_general_curvature(curvdef,cparams,L);
@@ -31,6 +43,31 @@ function [B,h] = fatbackbone_from_general_curvature(curvdef,cparams,L,width,orie
             
             % Multiply inverse of this matrix by point location matrix
             
+            B_aug = (R\B_aug')';
+            
+        case 'from-sys'
+		
+            % Import the datafile of the specified system
+            load(calcfilePath,'s');
+            
+            % Calculate tolerance for find() (this will vary depending on
+                                            % system evaluation density)
+            tol=abs(s.grid.eval{1}(1,1)-s.grid.eval{1}(2,1))*0.5;
+            
+            % Determine the location of the specified values of a1 and a2
+            [k,j]=find(abs(s.grid.eval{1}-r(1))<tol); % find indicies where a1 is as specified
+            row=k(1); % because a1 is represented in the x axis, k will be a vector of identical values. save just one
+            [k,j]=find(abs(s.grid.eval{2}(1,j)-r(2))<tol); % find the indicies where a1 AND a2 are as specified
+            col=j(1);
+            x = s.B_optimized.eval.Beta{1}(row,col); % find the value of Bx for the given a1 and a2
+            y = s.B_optimized.eval.Beta{2}(row,col); % find the value of By for the given a1 and a2
+            theta = s.B_optimized.eval.Beta{3}(row,col); % find the value of Btheta for the given a1 and a2
+            
+            %%% Same as in com-mean:%%%
+            R = [cos(theta) -sin(theta) x;
+                sin(theta) cos(theta) y;
+                0 0 1];
+            % Multiply inverse of this matrix by point location matrix
             B_aug = (R\B_aug')';
             
             
