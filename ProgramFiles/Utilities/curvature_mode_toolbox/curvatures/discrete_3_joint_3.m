fa6ea8595836daacbf13ac7a11612ab54639851c
Ross Hatton <rlhatton@graf317-01.local>
2018-09-01 00:55:49 -0700

    Improved accuracy of comments on discrete_3 curvature modes

--- ProgramFiles/Utilities/curvature_mode_toolbox/curvatures/discrete_3_joint_3.m
+++ ProgramFiles/Utilities/curvature_mode_toolbox/curvatures/discrete_3_joint_3.m
@@ -1,12 +1,13 @@
 function [kappa, joint_loc] = discrete_3_joint_3(s)
-% Curvature and integral of curvature for a discrete joint two thirds of the
+% Curvature and integral of curvature for a discrete joint 3/4 of the
 % way down the body, specified relative to a total body length of 1 unit,
 % centered at 1/2
 %
-% By returning joint_loc as a second output, this basis function instructs
-% the integrator to stop and restart at the discontinuity
+% By returning joint_loc as a second output, this basis function provides
+% an integral of the curvature that is more robust and faster to compute
+% than directly integrating a Dirac delta function.
 
-%joint is at 1/3 way from midpoint to tail end
+%joint is at 1/2 way from midpoint to tail end
 joint_loc = 1/4;
 
 % Curvature 
