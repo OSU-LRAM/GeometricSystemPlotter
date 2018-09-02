6aec26692fc4e0af8474fd304e8c646e02d647a1
Ross Hatton <rlhatton@graf317-01.local>
2018-09-02 00:11:39 -0700

    removed old names for N_link_chain and associated files

--- ProgramFiles/Utilities/curvature_mode_toolbox/n_link_kinematics.m
+++
@@ -1,7 +0,0 @@
-function [h,J] = n_link_kinematics(linklengths,jointangles)
-% Return the link positions and Jacobian to body velocities as a function
-% of link lengths and joint angles
-
-
-%%%%%%
-% First,
