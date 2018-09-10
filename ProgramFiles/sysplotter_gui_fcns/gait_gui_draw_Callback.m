9a106c65ef7254bd0447e9d60007027fcd7e77b3
rlhatton <Ross.Hatton@oregonstate.edu>
2018-09-10 16:09:50 -0700

    Draw gait now works with the last plot button pushed

--- ProgramFiles/sysplotter_gui_fcns/gait_gui_draw_Callback.m
+++ ProgramFiles/sysplotter_gui_fcns/gait_gui_draw_Callback.m
@@ -4,8 +4,11 @@ function gait_gui_draw_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
+% Get the last plot pushbutton used
+lastpushbutton = handles.figure1.UserData.lastpushbutton;
+
 % Plot whatever gaits are selected in the third checkbox column
-plot_info = plotpushbutton_Callback(findall(0,'tag','plotpushbutton3'), eventdata, handles);
+plot_info = plotpushbutton_Callback(findall(0,'tag',lastpushbutton), eventdata, handles);
 
 % Execute the gait_gui_draw command
     gait_gui_draw(plot_info(1).axes(1),hObject, eventdata, handles);
