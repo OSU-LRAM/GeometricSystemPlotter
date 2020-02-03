01c933ff609c79094d26ed1fd4a977f796afe8aa
Quinten Konyn <crystalsolenoid@gmail.com>
2020-02-03 13:20:29 -0800

    Applied ODE solver; started animation.
    I think the animation is working correctly. It plays as a popup, or can
    be saved as a .avi.
    
    Things to do:
    -[ ] play around with different gaits
    -[ ] have body origin path traced out during animation
    -[ ] add motion blur
    -[ ] test with no-scales snake (physics check)
    -[ ] compare animation to physical intuition
      (make sure scales are going in the correct direction)
    -[ ] move animation to function
    -[ ] store gaits so it's easy to manage multiple (gait files?)

--- ProgramFiles/AsymmetricFriction/apply_piecewise_system.m
+++ ProgramFiles/AsymmetricFriction/apply_piecewise_system.m
@@ -1,15 +1,20 @@
-function [body_velocity] = apply_piecewise_system(s, piecewise_system_map, adot_grid, i, j)
+function [body_velocity] = apply_piecewise_system(s, piecewise_system_map, shape, shapechange)
 %APPLY_PIECEWISE_SYSTEM Summary of this function goes here
 %   Detailed explanation goes here
 
-a_grid = s.grid.eval{1};
+% For now, determine dir according to the sign of a and adot
+% using results found for two-link.
 
 % pick which system to use for this a/adot
-dir = piecewise_system_map(i, j) - 1;
-backwards = [mod(dir,2), floor(dir/2)]; % this is a hacky way to do all 4 direction combos with binary
+opening = sign(shape) == sign(shapechange);
+if opening
+    dir = 3 - 1;
+else
+    dir = 2 - 1;
+end
+% dir = piecewise_system_map(i, j) - 1;
 
-shape = a_grid(i);
-shapechange = adot_grid(j);
+backwards = [mod(dir,2), floor(dir/2)]; % this is a hacky way to do all 4 direction combos with binary
 
 % get A(alpha)
 [A, ~, ~, ~, ~] = LowRE_connection_discrete(s.geometry, s.physics, shape, backwards);
