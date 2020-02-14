04184d09a8d5e34bb546d2ea3330e3e205b742ab
Quinten Konyn <crystalsolenoid@gmail.com>
2020-02-14 11:50:01 -0800

    Moved getting the correct A to its own function
    Started plotting A(alpha) in live script.

--- ProgramFiles/AsymmetricFriction/apply_piecewise_system.m
+++ ProgramFiles/AsymmetricFriction/apply_piecewise_system.m
@@ -7,20 +7,10 @@ function [body_velocity] = apply_piecewise_system(s, piecewise_system_map, shape
 
 % pick which system to use for this a/adot
 opening = sign(shape) == sign(shapechange);
-if opening
-    dir = 3 - 1;
-else
-    dir = 2 - 1;
-end
-% dir = piecewise_system_map(i, j) - 1;
 
-backwards = [mod(dir,2), floor(dir/2)]; % this is a hacky way to do all 4 direction combos with binary
-
-% get A(alpha)
-[A, ~, ~, ~, ~] = LowRE_connection_discrete(s.geometry, s.physics, shape, backwards);
+A = get_correct_A(s, shape, opening);
 
 % gcirc right
 body_velocity = -A * shapechange;
 
 end
-
---
+++ ProgramFiles/AsymmetricFriction/get_correct_A.m
@@ -0,0 +1,16 @@
+function A = get_correct_A(s, shape, opening)
+%GET_CORRECT_A Summary of this function goes here
+%   Detailed explanation goes here
+
+if opening
+    dir = 3 - 1;
+else
+    dir = 2 - 1;
+end
+
+backwards = [mod(dir,2), floor(dir/2)]; % this is a hacky way to do all 4 direction combos with binary
+
+% get A(alpha)
+[A, ~, ~, ~, ~] = LowRE_connection_discrete(s.geometry, s.physics, shape, backwards);
+end
+
