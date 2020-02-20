b0a3612849f8515d81b39aa8e05e7189e23a7c94
Quinten Konyn <crystalsolenoid@gmail.com>
2020-02-20 14:41:58 -0800

    Buffer spacing added between slopes

--- ProgramFiles/AsymmetricFriction/slope_plot.m
+++ ProgramFiles/AsymmetricFriction/slope_plot.m
@@ -3,6 +3,7 @@ function slope_plot(grid,A,style)
 %   Detailed explanation goes here
 
 % NaN in plot makes a line break
+buffer_ratio = 0.3;
 
 slope_x_pos = zeros([(length(grid)-1)*3, 1]);
 slope_x_neg = zeros(size(slope_x_pos));
@@ -10,7 +11,9 @@ slope_y_pos = zeros([(length(grid)-1)*3, 3]);
 slope_y_neg = zeros(size(slope_y_pos));
 for i=1:length(grid)-1
     j = (i - 1) * 3 + 1;
-    da = grid(i+1) - grid(i);
+    spacing = grid(i+1) - grid(i);
+    buffer = spacing * buffer_ratio;
+    da = spacing - buffer;
     if style
     % one way:
         slope_x_pos(j:j+2) = [grid(i), grid(i)+da/2, NaN];
@@ -39,7 +42,7 @@ figure()
 tiledlayout(3,1)
 for i=1:3
     ax = nexttile;
-    plot(ax, slope_x_pos, slope_y_pos(:,i), 'k', slope_x_neg, slope_y_neg(:,i), 'r');
+    plot(ax, slope_x_pos, slope_y_pos(:,i), 'r', slope_x_neg, slope_y_neg(:,i), 'k');
 end
 
 end
