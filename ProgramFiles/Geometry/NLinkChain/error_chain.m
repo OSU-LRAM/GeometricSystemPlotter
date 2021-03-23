e0ef6c3db67065f2308261524a62ae30d534cb85
Capprin Bass <capprin@gmail.com>
2021-03-23 10:43:48 -0600

    Added 'truncate_to' term for finer error control; added default outputs (J, configurations)

--- ProgramFiles/Geometry/NLinkChain/error_chain.m
+++ ProgramFiles/Geometry/NLinkChain/error_chain.m
@@ -1,18 +1,43 @@
 % does chain construction and error visualization using GroupElements
-function end_error_lae = error_chain(geometry, configuration, order)
+function [end_error_lae,...
+          end_error_lge,...
+          expected_terms,...
+          sep_error_terms,...
+          joint_configurations,...
+          J] = error_chain(geometry, configuration, order, truncate_to)
     % inputs:
         % geometry:
             % elements: cell array of GroupElements encoding transforms
             % types: cell array of characters encoding transform types
                 % 'j': joint
                 % 'e': error
-                % 'l': link (can really be anything)
+                % 'l': link
         % configuration: all scalar/sym arrays, multiplying their
         % respective transform types
             % links
             % joints
             % errors
         % order: integer order for both Taylor Series and BCH approx.
+        % truncate_to: integer degree for term truncation
+    % outputs:
+        % end_error_lae:
+            % AlgebraElement encoding the joint error propogated to the end
+            % effector
+        % end_error_lge:
+            % GroupElement after exponentiation of end_error_lae using
+            % taylor series approximation to supplied order
+        % expected_terms:
+            % cell array of polynomial terms in any supplied error 
+            % variables, up to degree supplied by truncate_to
+        % sep_error_terms:
+            % cell array of terms equivalent to end_error_lge, but 
+            % separated into quantities manifested as expected_terms
+        % joint_configurations:
+            % matrix of nominal world space configurations of supplied
+            % joints
+        % J:
+            % 3D array (pages of matrices) containing Jacobians of supplied
+            % joints
     
     %% input sanitation
     % geometry checking
@@ -32,6 +57,10 @@ function end_error_lae = error_chain(geometry, configuration, order)
         warning('error_chain: order not provided; assuming order 1');
         order = 1;
     end
+    if ~exist('truncate_to', 'var')
+        warning('error_chain: truncate_to not provided; truncated to degree 1');
+        truncate_to = 1;
+    end
     % create logical arrays for easy indexing
     link_logical = cellfun(@(c) strcmp(c,'l'), geometry.types);
     joint_logical = cellfun(@(c) strcmp(c,'j'), geometry.types);
@@ -41,13 +70,16 @@ function end_error_lae = error_chain(geometry, configuration, order)
         configuration = struct;
     end
     if any(link_logical) && ~isfield(configuration, 'links')
+        % assume links are as-given
         configuration.links = ones(1, sum(link_logical));
     end
     if any(joint_logical) && ~isfield(configuration, 'joints')
-        configuration.joints = ones(1, sum(joint_logical));
+        % assume reference for joints
+        configuration.joints = zeros(1, sum(joint_logical));
     end
     if any(error_logical) && ~isfield(configuration, 'errors')
-        configuration.errors = ones(1, sum(error_logical));
+        % assume reference for errors
+        configuration.errors = zeros(1, sum(error_logical));
     end
     
     %% construction of Lie Algebra elements
@@ -75,30 +107,125 @@ function end_error_lae = error_chain(geometry, configuration, order)
     algebras(joint_logical) = joints;
     algebras(error_logical) = errors;
     
-    %% symbolic math for error approximation
-    % create symbolic error term
-    dim = length(algebras{1}.vector);
-    sigma_vec = sym('sigma', [1, dim]);
-    sigma = AlgebraElement(sigma_vec);
-    % use BCH to write LHS, RHS
-    lhs_elements = [algebras(~error_logical) {sigma}];
-    LHS = BCH_n_elements(lhs_elements, order);
-    RHS = BCH_n_elements(algebras, order);
-    % solve for error, sigma
-    sigma_soln_struct = solve(LHS.vector == RHS.vector, sigma_vec);
-    sigma_soln_vec = subs(sigma_vec, sigma_soln_struct);
-    end_error_lae = AlgebraElement(sigma_soln_vec);
-    % exponentiate with T.S. approx.
-    end_error_lge = end_error_lae.to_GroupElement_Taylor(order);
-    
-    %% break solution apart in terms of errors
-    % break into terms: linear, bilinear, quadratic... (in ea. err)
-    % plot contributions (in an interactable plot environment?)
-    
-    % existing outputs:
-        % link configurations
-        % Jacobians of each link
-    % new outputs:
-        % error term(s)?
-        % plotted errors
+    %% error math
+    % check for error terms before jumping in
+    if any(error_logical)
+        %% symbolic math for error approximation
+        % create symbolic error term
+        dim = length(algebras{1}.vector);
+        sigma_vec = sym('sigma', [1, dim]);
+        assume(sigma_vec, 'real');
+        sigma = AlgebraElement(sigma_vec);
+        % use BCH to write LHS, RHS
+        lhs_elements = [algebras(~error_logical) {sigma}];
+        LHS = BCH_n_elements(lhs_elements, order);
+        RHS = BCH_n_elements(algebras, order);
+        % solve for error, sigma
+        sigma_soln_struct = solve(LHS.vector == RHS.vector, sigma_vec);
+        sigma_soln_vec = subs(sigma_vec, sigma_soln_struct);
+        end_error_lae = AlgebraElement(sigma_soln_vec);
+        % exponentiate
+        end_error_lge = end_error_lae.to_GroupElement_Taylor(order);
+        end_error_lge = end_error_lge.vector_small_angles();
+        %% create polynomial of error terms (to search)
+        % get supplied error variables vector
+        err_vars = [];
+        for e = 1:length(errors)
+            curr_vars = symvar(errors{e}.matrix);
+            exist_vars = ismember(curr_vars, err_vars);
+            err_vars = [err_vars curr_vars(~exist_vars)];
+        end
+        % create possible polynomial terms
+        full_poly = 1;
+        for v = 1:length(err_vars)
+            tmp_poly = 1;
+            for d = 1:truncate_to
+                tmp_poly = tmp_poly + err_vars(v)^d;
+            end
+            full_poly = full_poly*tmp_poly;
+        end
+        full_poly = expand(full_poly);
+        % represent as ind. terms; truncate to desired degree
+        expected_terms = children(full_poly);
+        t = 1;
+        while t <= length(expected_terms)
+            if polynomialDegree(expected_terms{t}) > truncate_to
+                expected_terms(t) = [];
+            else
+                t = t+1;
+            end
+        end
+        %% search for expected polynomial terms
+        % get terms in aggregate error matching expected terms
+        end_error_poly = expand(end_error_lge.vector);
+        end_error_terms = children(end_error_poly);
+        sep_error_terms = cell(size(expected_terms));
+        % searching for expected terms
+        for term = 1:length(expected_terms)
+            contribution = sym(zeros(size(end_error_terms,1),1));
+            % search along ea. dimension
+            for dim = 1:length(end_error_terms)
+                % inspect ea. term of full end error polynomial
+                for t = 1:length(end_error_terms{dim})
+                    unit_quantity = end_error_terms{dim}{t}/expected_terms{term};
+                    symvars = symvar(unit_quantity);
+                    % if this is correct term, should be no err. terms after
+                    % division
+                    curr_exist_err_vars = ismember(symvars, err_vars);
+                    if ~any(curr_exist_err_vars)
+                        contribution(dim) = contribution(dim) + end_error_terms{dim}{t};
+                    end
+                end
+            end
+            sep_error_terms{term} = contribution;
+        end
+    else
+        % no error terms; set outputs
+        end_error_lae = AlgebraElement(zeros(size(joints{1}.vector)));
+        end_error_lge = GroupElement(zeros(size(end_error_lae.vector)));
+        expected_terms = 0;
+        sep_error_terms = 0;
+    end
+    %% produce standard N_link_chain outputs (link configs, jacobians)
+    % pre-allocate storage for joint configs, Jacobians
+    joint_configurations = zeros(length(joints), length(joints{1}.vector));
+    J = zeros(size(joint_configurations, 2),...
+              size(joint_configurations,1),...
+              size(joint_configurations,1));
+    % keep track of current configuration (start as identity)
+    current_config = GroupElement(zeros(1,size(joint_configurations, 2)));
+    % count link/joint (for scalar indexing)
+    j = 1;
+    l = 1;
+    % iterate thru group elements
+    for e = 1:length(geometry.elements)
+        % if error, skip
+        if geometry.types{e} == 'e'
+            continue
+        % if link or joint, scale by supplied scalars
+        elseif geometry.types{e} == 'l'
+            trans_vec = geometry.elements{e}.vector*configuration.links(l);
+            l = l + 1;
+        elseif geometry.types{e} == 'j'
+            trans_vec = geometry.elements{e}.vector*configuration.joints(j);
+            % not incrementing j; will do later
+        end
+        % apply scaled transform to current config.
+        transform = GroupElement(trans_vec);
+        current_config = current_config.right_action(transform);
+        % for joints, save relevant data
+        if geometry.types{e} == 'j'
+            % save configuration
+            [current_config, conf_vec] = current_config.vector_true();
+            joint_configurations(j,:) = conf_vec';
+            % construct columns of J w.r.t base link
+            % done vector-wise (not group-wise) because I have a slow brain
+            for j_prev = 1:(j-1)
+                diff_vec = conf_vec' - joint_configurations(j_prev,:);
+                J(:, j_prev, j) = cross(diff_vec, joints{j_prev}.vector);
+            end
+            % increment joint index
+            j = j + 1;
+        end
+    end
 end
