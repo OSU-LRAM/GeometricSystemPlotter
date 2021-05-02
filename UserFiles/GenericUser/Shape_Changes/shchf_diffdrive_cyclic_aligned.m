9b8908742ada1cf4e70dc7726912a3625a4d529d
Capprin Bass <capprin@gmail.com>
2021-05-02 15:18:44 -0700

    Added cyclic diffdrive shape changes, as well as a shape-axis-aligned diffdrive system

---
+++ UserFiles/GenericUser/Shape_Changes/shchf_diffdrive_cyclic_aligned.m
@@ -0,0 +1,63 @@
+function output = shchf_diffdrive_cyclic(input_mode,pathnames)
+	% Default argument
+    if ~exist('input_mode','var')
+		input_mode = 'initialize';
+    end
+    
+    switch input_mode
+		case 'name'
+			output = 'Diffdrive Cyclic Motion, Aligned';
+		case 'dependency'
+			output.dependency = {};
+		case 'initialize'
+            % path definition
+            % equivalent to example motion, but cyclic (parallel parking)
+            side_lengths = pi/32:pi/64:pi/8;
+            p.phi_def = cell(1,length(side_lengths));
+            for i = 1:length(side_lengths)
+                p.phi_def{i} = {@(t) drive_forward(t, side_lengths(i)),...
+                                @(t) turn_left(t, side_lengths(i)),...
+                                @(t) drive_backward(t, side_lengths(i)),...
+                                @(t) turn_right(t, side_lengths(i))};
+            end
+            
+			%marker locations
+			p.phi_marker = []; % No marker on this path (can put, e.g. endpoints of path if desired)
+			
+			%arrows to plot
+			p.phi_arrows = {{1,1,1,1}};
+
+			%time to run path
+			p.time_def = [0 1]; % Duration of each segment
+
+			% enable area integration
+			p.cBVI_method{1}{1} = 'simple';
+
+			%number of points in each path.
+			p.phi_res = 20;
+
+			% output the path properties
+			output = p;
+    end
+end
+
+function [alpha] = drive_forward(t,len)
+	t = t(:)*len;
+    start = [0 0];
+	alpha = start + [t zeros(size(t))];
+end
+
+function [alpha] = turn_left(t,len)
+	t = t(:)*len;
+	alpha = drive_forward(1,len) + [zeros(size(t)) t];
+end
+
+function [alpha] = drive_backward(t,len)
+    t = t(:)*len;
+    alpha = turn_left(1,len) + [-t zeros(size(t))];
+end
+
+function [alpha] = turn_right(t,len)
+    t = t(:)*len;
+    alpha = drive_backward(1,len) + [zeros(size(t)) -t];
+end
---
+++ UserFiles/GenericUser/Systems/sysf_diffdrive_aligned.m
@@ -0,0 +1,84 @@
+function output = sysf_diffdrive(input_mode,pathnames)
+%Return the system properties for a diffdrive car
+
+	% Default arguments
+	if ~exist('input_mode','var')
+		
+		input_mode = 'name';
+		
+	end
+		
+	%%%%%%%
+	
+	switch input_mode
+
+		case 'name'
+
+			output = 'Diffdrive Car, Aligned'; % Display name
+
+		case 'dependency'
+
+			output.dependency = {}; 
+			% For system files depending on other files in the Systems
+			% directory, list those filenames here for the dependency
+			% checker
+
+		case 'initialize'
+
+			% Differential dr
+            
+			%%%
+			% Local connection (functions at end of file)
+			s.A_num = @Conn_num;
+
+
+			%%%
+			%Processing details
+
+			%Range over which to evaluate connection
+			s.grid_range = [-1,1,-1,1]*2.5;
+
+			%densities for various operations
+			s.density.vector = [10 10]; %density to display vector field
+			s.density.scalar = [51 51]; %density to display scalar functions
+			s.density.eval = [21 21];   %density for function evaluations
+            s.density.finite_element=31;
+
+			%%%
+			%Display parameters
+
+			%shape space tic locations
+			s.tic_locs.x = [-1 0 1];
+			s.tic_locs.y = [-1 0 1];
+
+
+			%Don't optimize the reference point (turn this off for
+			%non-carlike systems)
+			s.xy_no_opt = 1;
+            
+            % Set system type variable for gait optimization
+            s.system_type = 'inertia';
+
+			%%%%
+            
+			%Output the system properties
+			output = s;
+		
+	end
+    
+end
+
+function A_num = Conn_num(a1,a2)
+% Numerator of the local connection (in this case, no denominator is needed). 
+%
+% Note that the function returns a single large array of values for the
+% local connection:
+% 
+% if a1 and a2 are each NxM matrices of joint angles, A is a 3x2 collection
+% of NxM matrices, each corresponding to the i,jth component of A
+
+	A_num = [-ones(size(a1)) zeros(size(a1));
+             zeros(size(a1)) zeros(size(a1));
+             zeros(size(a1)) -ones(size(a1))];
+	
+end
